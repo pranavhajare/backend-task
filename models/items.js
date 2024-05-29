5352e27c68fe3623dd6b8ca503198a33dbb616b2
pranavhajare <hajare.pranav@gmail.com>
2024-05-29 15:52:34 +0530

    Add item model

---
+++ models/items.js
@@ -0,0 +1,14 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/config");
+
+const Item = sequelize.define("Item", {
+  name: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.TEXT, allowNull: false },
+  starting_price: { type: DataTypes.DECIMAL, allowNull: false },
+  current_price: { type: DataTypes.DECIMAL, defaultValue: DataTypes.DECIMAL },
+  image_url: { type: DataTypes.STRING },
+  end_time: { type: DataTypes.DATE, allowNull: false },
+  created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+});
+
+module.exports = Item;
